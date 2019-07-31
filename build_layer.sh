0d00c7034611d8e5fc7adfbcab9466092fb09ef6
Frederick Cheung <frederick.cheung@gmail.com>
2019-07-31 17:53:56 +0100

    setup so that python libs are also installed

--- build_layer.sh
+++ build_layer.sh
@@ -3,8 +3,12 @@
 set -e
 mkdir -p pg-layer/lib
 
+mkdir python
+pip install PygreSQL --no-deps -t python
+
 # Copy lib files from postgres to lib directory
 cp postgresql/tmp_install/usr/local/pgsql/lib/libpq.so* pg-layer/lib
+cp -R python pg-layer
 
 # Zip the layer
 cd pg-layer
---
+++ install_dependencies.sh
@@ -0,0 +1,1 @@
+yum install -q -y git-core gcc readline-devel make zlib-devel perl flex bison zip
