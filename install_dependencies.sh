c48d31fb96b7f78467037f03b98df09d0b7744b0
Frederick Cheung <frederick.cheung@gmail.com>
2019-07-31 17:55:00 +0100

    more deps

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,1 +1,1 @@
-yum install -q -y git-core gcc readline-devel make zlib-devel perl flex bison zip
+yum install -q -y git-core gcc readline-devel make zlib-devel perl flex bison zip python2-pip python2-devel
